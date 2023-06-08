e9b9516598080dbdfb1378152fc9a2803e84c887
Jakob Brattke <73968859+jbrattke@users.noreply.github.com>
2023-06-07 21:58:17 -0400

    init commit

---
+++ Scripts/MouseLook.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    Transform playerBody;
+    public float mouseSensitivity = 400f;
+    public static bool isUIActive; 
+    float pitch = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = transform.parent.transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isUIActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isUIActive)
+        {
+            float moveX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            // Apply yaw rotation to player body
+            playerBody.Rotate(Vector3.up * moveX);
+
+            // Apply pitch to camera
+            pitch -= moveY;
+
+            // limit player from rotating all the way upside down
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        }
+    }
+
+    public static void ToggleUI()
+    {
+        isUIActive = !isUIActive;
+
+        if (isUIActive)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
